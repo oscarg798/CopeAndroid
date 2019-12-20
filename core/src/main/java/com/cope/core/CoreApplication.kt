1437490b0211da04d895f6d952ce81b94bcf40bc
Oscar Gallon <oscarg798@gmail.com>
2019-12-20 21:05:22 +0100

    fixing issues

--- core/src/main/java/com/cope/core/CoreApplication.kt
+++ core/src/main/java/com/cope/core/CoreApplication.kt
@@ -1,16 +1,23 @@
 package com.cope.core
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import com.cope.core.di.*
 import com.cope.core.exceptions.CoreComponentNotInitializatedException
+import com.google.firebase.FirebaseApp
 import com.squareup.picasso.Picasso
 
 /**
  * @author Oscar Gallon on 2019-06-06.
  */
-class CoreApplication : Application(), CoreComponentProvider {
+class CoreApplication : MultiDexApplication(), CoreComponentProvider {
 
     private var coreComponent: CoreComponent? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
 
     override fun getCoreComponent(): CoreComponent {
         if (coreComponent == null) {
--- core/src/main/java/com/cope/core/di/TrackingInterceptor.kt
+++ core/src/main/java/com/cope/core/di/TrackingInterceptor.kt
@@ -1,6 +1,8 @@
 package com.cope.core.di
 
+import com.google.firebase.FirebaseApp
 import com.google.firebase.perf.FirebasePerformance
+import com.google.firebase.perf.metrics.HttpMetric
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Request
@@ -8,21 +10,29 @@ import okhttp3.Response
 
 class TrackingInterceptor : Interceptor {
 
+    private var metric: HttpMetric? = null
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
+        startTracking(request)
+        val response = chain.proceed(request)
+        stopMetricts(response.code())
+        return response
+    }
+
+    private fun startTracking(request: Request) {
         val (url: HttpUrl, method: String) = request
-        val metric = FirebasePerformance.getInstance().newHttpMetric(
+        metric = FirebasePerformance.getInstance().newHttpMetric(
             url.toString(),
             method
         )
-        metric.start()
-        metric.setRequestPayloadSize(request.body()?.contentLength() ?: 0)
-        val response = chain.proceed(request)
-        metric.setHttpResponseCode(response.code())
-        metric.stop()
+        metric?.setRequestPayloadSize(request.body()?.contentLength() ?: 0)
+        metric?.start()
+    }
 
-        return response
+    private fun stopMetricts(responseCode: Int) {
+        metric?.setHttpResponseCode(responseCode)
+        metric?.stop()
     }
 
 
